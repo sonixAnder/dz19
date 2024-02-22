a4a69058366bd3da36c615c0fbd3249a06313a13
sonixAnder <148888208+sonixAnder@users.noreply.github.com>
2024-02-22 16:15:06 +0300

    Add files via upload

---
+++ copying and reverse order/copying and reverse order/copying and reverse order.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int arr1[] = { 1, 2, 3, 4, 5 };
+    int arr2[5];
+
+    int* p1 = arr1;
+    int* p2 = arr2;
+
+    for (int i = 0; i < 5; i++) 
+    {
+        *p2 = *(p1 + 4 - i);
+        p2++;
+    }
+
+    for (int i = 0; i < 5; i++) 
+    {
+        cout << arr2[i] << " ";
+    }
+
+    return 0;
+}
+
---
+++ reverse the order of the array elements/reverse the order of the array elements/reverse the order of the array elements.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+void reverseArray(int* arr, int size) 
+{
+    int* start = arr; 
+    int* end = arr + size - 1; 
+
+    while (start < end) 
+    {
+        
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "ru");
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Оригинальный массив: ";
+    for (int i = 0; i < size; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+
+    reverseArray(arr, size);
+
+    cout << "\nПротивоположный: ";
+    for (int i = 0; i < size; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
